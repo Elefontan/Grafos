6aa1b3339209660ea77d4653880ad4bf176470a1
Elefontan <elena.fontan.alende@gmail.com>
2019-11-26 20:38:53 +0100

    Packages

---
+++ packages.R
@@ -0,0 +1,15 @@
+
+#Installing packages
+install.packages("igraph")
+install.packages("snahelper")
+install.packages("ggplot2")
+install.packages("ggraph")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("gridSVG")
+install.packages("XML")
+install.packages("svglite")
+install.packages("RColorBewer") #not available for R version 3.6.1
+install.packages("scales")
+install.packages("ape")
+install.packages("ggtree")  #not available for R version 3.6.1
